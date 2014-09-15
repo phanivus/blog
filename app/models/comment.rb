ddaa096b48db1fe9ea3ef634de571b9615fc8e52
phanivus <phanivus@gmail.com>
2014-09-15 14:37:39 +0530

    Added Authentication

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
+require "active_support"
+
 class Comment < ActiveRecord::Base
+		include ActiveModel::Validations
   belongs_to :post
+  validates_presence_of :post_id
+  validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
-class Post < ActiveRecord::Base
+require "active_support"
+
+class Post < ActiveRecord::Base 
+		include ActiveModel::Validations
   has_many :comments, dependent: :destroy
+  validates_presence_of :title
+  validates_presence_of :body 
 end
